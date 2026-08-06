6aaf1f77b5501a94a85c9f09c7d06435598f090d
Hervé Audren <herve.audren@woven-planet.global>
2026-08-06 17:20:45 +0900

    Fix up typing

--- lint_launch/lint_launch/xml_tools.py
+++ lint_launch/lint_launch/xml_tools.py
@@ -1,20 +1,24 @@
 # Copyright 2026 Toyota Motor Corporation
 
 import functools
+import io
 import xml.etree.ElementTree as ET
 
 import launch
 import launch_xml
+from launch.frontend.entity import Entity
 from launch.frontend.expose import instantiate_action
-from launch_xml.entity import Entity
+from launch.frontend.parser import Parser
+from launch_xml.entity import Entity as XMLEntity
+from launch_xml.parser import Parser as XMLParser
 
 
 class LineNumberingParser(ET.XMLParser):
-    def __init__(self, file_name: str | None):
+    def __init__(self, file_name: str | None) -> None:
         super().__init__()
         self.file_name = file_name
 
-    def _start(self, *args, **kwargs):
+    def _start(self, *args, **kwargs) -> ET.Element:
         element = super(self.__class__, self)._start(*args, **kwargs)
         element._file_name = self.file_name
         element._start_line_number = self.parser.CurrentLineNumber
@@ -23,14 +27,14 @@ class LineNumberingParser(ET.XMLParser):
         return element
 
 
-def load(cls, file):
+def load(cls: type[XMLParser], file: str | io.TextIOBase) -> tuple[XMLEntity, XMLParser]:
     file_name = getattr(file, "name", None)
     parser = LineNumberingParser(file_name)
-    return (Entity(ET.parse(file, parser=parser).getroot()), cls())
+    return (XMLEntity(ET.parse(file, parser=parser).getroot()), cls())
 
 
 @functools.wraps(instantiate_action)
-def my_instantiation(entity, parser):
+def my_instantiation(entity: Entity, parser: Parser) -> launch.Action:
     action = instantiate_action(entity, parser)
     if hasattr(entity, "_Entity__xml_element"):
         action.__location__ = (
@@ -40,6 +44,6 @@ def my_instantiation(entity, parser):
     return action
 
 
-def register_xml_hooks():
+def register_xml_hooks() -> None:
     launch_xml.parser.Parser.load = classmethod(load)
     launch.frontend.parser.instantiate_action = my_instantiation
